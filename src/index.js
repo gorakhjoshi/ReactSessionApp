b0ac94ec4aede95a91f966c070adca0123177fc5
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-02-08 21:48:07 +0545

    useState used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 function App() {
-  return <div>Hello World</div>;
+  const [state, setState] = useState(0);
+  console.log(state);
+
+  const increment = () => {
+    setState(state + 1);
+  };
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      <h3>Incremented Value: {state}</h3>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
